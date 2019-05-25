676c5dafbecfff3a786438d73aa68e2e5ed189f7
walkeknow <kewalshah077@gmail.com>
2019-05-26 01:35:39 +0530

    Implement Basic Card Flip Animation and Dictionary API Retrieval

--- vocab/views.py
+++ vocab/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+import os
+from django.conf import settings
+import urllib.parse
+import requests
+import re
 
 
 # Create your views here.
@@ -7,4 +12,33 @@ def index(request):
 
 
 def test(request, test_id):
-    return render(request, 'test.html', {'test': test_id})
+    if request.method == 'POST':
+        word = request.POST['current_word']
+        key = 'f97dc13c-a75e-4a85-b40e-76e2c3552a5a'
+        syn_key = '0baeb92e-a2d9-455d-a678-aa0586a53e95'
+        main_api = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/'
+        syn_api = 'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/'
+        url = main_api + word + '?' + urllib.parse.urlencode({
+            'key': key
+        })
+        syn_url = syn_api + word + '?' + urllib.parse.urlencode({
+            'key': syn_key
+        })
+        print(url)
+        print(syn_url)
+        json = requests.get(url).json()
+        syn_json = requests.get(syn_url).json()
+        definitions = json[0]['shortdef']
+        synonyms = syn_json[0]['meta']['syns'][0]
+        antonyms = syn_json[0]['meta']['ants'][0]
+        print(definitions)
+        print(synonyms)
+        print(antonyms)
+    word_file = open(os.path.join(settings.BASE_DIR, 'staticfiles/' + 'temp.txt'))
+    word_list = word_file.read().splitlines()
+    print(word_list)
+    context = {
+        "test": test_id,
+        "words": word_list
+    }
+    return render(request, 'test.html', context)
