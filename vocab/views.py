c4940d8b9ba693af90ae2e1b9d1d0ac64d8b0421
walkeknow <kewalshah077@gmail.com>
2019-05-28 02:09:41 +0530

    Implement Card Flip Backend

--- vocab/views.py
+++ vocab/views.py
@@ -4,7 +4,7 @@ from django.conf import settings
 import urllib.parse
 import requests
 import random
-
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
@@ -14,33 +14,65 @@ def index(request):
 def test(request, test_id):
     if request.method == 'POST':
         word = request.POST['current_word']
-        key = 'f97dc13c-a75e-4a85-b40e-76e2c3552a5a'
+        print(word)
+        # key = 'f97dc13c-a75e-4a85-b40e-76e2c3552a5a'
         syn_key = '0baeb92e-a2d9-455d-a678-aa0586a53e95'
-        main_api = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/'
+        # main_api = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/'
         syn_api = 'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/'
-        url = main_api + word + '?' + urllib.parse.urlencode({
-            'key': key
-        })
+        # url = main_api + word + '?' + urllib.parse.urlencode({
+        #     'key': key
+        # })
         syn_url = syn_api + word + '?' + urllib.parse.urlencode({
             'key': syn_key
         })
-        print(url)
+        # print(url)
         print(syn_url)
-        json = requests.get(url).json()
+        # json = requests.get(url).json()
         syn_json = requests.get(syn_url).json()
-        definitions = json[0]['shortdef']
-        synonyms = syn_json[0]['meta']['syns'][0]
-        antonyms = syn_json[0]['meta']['ants'][0]
+        # definitions = json[0]['shortdef']
+        definitions = syn_json[0]['shortdef']
+        definitions = [word.capitalize() for word in definitions]
+        definitions = ";".join(definitions)
+        synonyms = ['No Synonyms Found']
+        if syn_json[0]['meta']['syns'][0] is not None:
+            all_synonyms = syn_json[0]['meta']['syns'][0]
+            synonyms = all_synonyms[:5]
+            synonyms = [word.capitalize() for word in synonyms]
         print(definitions)
         print(synonyms)
-        print(antonyms)
+        context = {
+            "meanings": definitions,
+            "synonyms": synonyms
+        }
+        return JsonResponse(context)
     word_file = open(os.path.join(settings.BASE_DIR, 'staticfiles/' + 'temp.txt'))
     lower_word_list = word_file.read().splitlines()
     word_list = [word.capitalize() for word in lower_word_list]
     random.shuffle(word_list)
     print(word_list)
+    word = word_list[0]
+    print(word)
+    syn_key = '0baeb92e-a2d9-455d-a678-aa0586a53e95'
+    syn_api = 'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/'
+    syn_url = syn_api + word + '?' + urllib.parse.urlencode({
+        'key': syn_key
+    })
+    print(syn_url)
+    syn_json = requests.get(syn_url).json()
+    definitions = syn_json[0]['shortdef']
+    definitions = [word.capitalize() for word in definitions]
+    definitions = ";".join(definitions)
+    synonyms = ['No Synonyms Found']
+    if syn_json[0]['meta']['syns'][0] is not None:
+        all_synonyms = syn_json[0]['meta']['syns'][0]
+        synonyms = all_synonyms[:5]
+        synonyms = [word.capitalize() for word in synonyms]
+    print(definitions)
+    print(synonyms)
     context = {
         "test": test_id,
-        "words": word_list
+        "words": word_list,
+        "first_meanings": definitions,
+        "first_synonyms": synonyms
     }
     return render(request, 'test.html', context)
