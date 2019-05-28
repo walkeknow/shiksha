6e958c67af079df17eb79b4b03bf5379b91edc97
walkeknow <kewalshah077@gmail.com>
2019-05-29 00:33:09 +0530

    Display words which cause Ajax errors for testing

--- vocab/static/js/card.js
+++ vocab/static/js/card.js
@@ -100,22 +100,22 @@ function execute(words, synonyms, meanings, csfr_token) {
                 error: function (jqXHR, exception) {
                     var msg = '';
                     if (jqXHR.status === 0) {
-                        msg = 'Not connect.\n Verify Network.';
+                        msg = 'Not connect.\n Verify Network';
                     } else if (jqXHR.status == 404) {
-                        msg = 'Requested page not found. [404]';
+                        msg = 'Requested page not found [404]';
                     } else if (jqXHR.status == 500) {
-                        msg = 'Internal Server Error [500].';
+                        msg = 'Internal Server Error [500]';
                     } else if (exception === 'parsererror') {
-                        msg = 'Requested JSON parse failed.';
+                        msg = 'Requested JSON parse failed';
                     } else if (exception === 'timeout') {
                         msg = 'Time out error.';
                     } else if (exception === 'abort') {
-                        msg = 'Ajax request aborted.';
+                        msg = 'Ajax request aborted';
                     } else {
-                        msg = 'Uncaught Error.\n' + jqXHR.responseText;
+                        msg = 'Uncaught Error\n' + jqXHR.responseText;
                     }
                     console.log(msg);
-                    alert("Error " + msg + " Please Try Again Later!")
+                    alert("msg"+ " processing the word " + word +". Please Try Again Later!")
                 },
             });
         }
