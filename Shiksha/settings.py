848331fd21e5cdb2fabd27fda2aef4d6655f3bdc
walkeknow <kewalshah077@gmail.com>
2019-05-29 19:42:39 +0530

    Add logo and update ogp details

--- Shiksha/settings.py
+++ Shiksha/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&3kg^=@sc3xrk((yzu%@5rp*qpn^2=)ote#l6!4do$(=)8vs)&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
